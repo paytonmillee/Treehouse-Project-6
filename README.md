a8525afa0cb02bdcb13b61a6f3b803df38ec2ce9
paytonmillee <74552655+paytonmillee@users.noreply.github.com>
2021-02-02 16:57:32 -0600

    final commit
    final commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This project presents a portfolie that displays a description of myself, my career path and a few of the projects I have completed. The projects can be viewed on my github repo and github live.
---
+++ app.js
@@ -0,0 +1,56 @@
+//making our application know we are using express
+
+const express = require("express");
+const data = require("./data.json");
+
+const app = express();
+const port = 3000;
+//Getting the application to use the pug templating engine
+app.set("view engine", "pug");
+//Getting the application to use a static folder
+app.use("/static", express.static("public"));
+//Requesting portfolio's index
+app.get("/", (req, res) => {
+  res.locals.projects = data.projects;
+  res.render("index");
+});
+//Requesting portfolio'sS about
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+//Getting the application to run the routes we have requested
+app.get("/project/:id", (req, res, next) => {
+  const id = req.params.id;
+  let project;
+  data.projects.map((projectData) => {
+    if (id == projectData.id) {
+      project = projectData;
+      console.log(projectData);
+    }
+  });
+  if (project) {
+    res.locals.project = project;
+    res.render("project");
+  } else {
+    next();
+  }
+});
+//Getting the application to show an error if page or route isn't able to be found
+app.use((req, res, next) => {
+  const err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+//Getting the application we are wanting to display an error page if one has occured
+app.use((err, req, res, next) => {
+  if (!err.status) {
+    err.status = 500;
+  }
+  res.locals.error = err;
+  res.status(err.status);
+  res.render("error");
+});
+//Getting application to run on port
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${3000}`);
+});
